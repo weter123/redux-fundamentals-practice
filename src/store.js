b61a4b015c68a9fbcd1efc7e5f3824be7c48efbf
weter123 <dull.x22@gmail.com>
2022-06-28 15:45:46 -0400

    added middleware to the composed enhancer imported applyMiddleware from 'redux' initialized middlewareEnhancer variable that uses applyMiddleware to apply three middleware enhancers: print1, print2, and print3. included middlewareEnhancer in composedEnhancer.

--- src/store.js
+++ src/store.js
@@ -1,11 +1,17 @@
-import { createStore, compose } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import {
   includeMeaningOfLife,
   sayHiOnDispatch
 } from "./exampleAddons/enhancers";
 import rootRuducer from "./reducer";
+import { print1, print2, print3 } from "./exampleAddons/middleware";
 
-const composedEnchancer = compose(sayHiOnDispatch, includeMeaningOfLife);
+const middlewareEnhancer = applyMiddleware(print1, print2, print3);
+const composedEnchancer = compose(
+  sayHiOnDispatch,
+  includeMeaningOfLife,
+  middlewareEnhancer
+);
 const store = createStore(rootRuducer, undefined, composedEnchancer);
 
 export default store;
