4309b446230dce3d21311bd8ae3aa4fd3676ad45
weter123 <dull.x22@gmail.com>
2022-06-28 16:22:41 -0400

    reduced dispatch to one example

--- src/index.js
+++ src/index.js
@@ -2,35 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from "./store";
+
 import "./api/server";
 
-console.log("initial state", store.getState());
-
-const unsubscribe = store.subscribe(() =>
-  console.log("state after dispatch", store.getState())
-);
+import store from "./store";
 
 store.dispatch({ type: "todos/todoAdded", payload: "Learn about actions" });
-store.dispatch({ type: "todos/todoAdded", payload: "Learn about reducers" });
-store.dispatch({ type: "todos/todoAdded", payload: "Learn about stores" });
-
-store.dispatch({ type: "todos/todoToggled", payload: 0 });
-store.dispatch({ type: "todos/todoToggled", payload: 1 });
-
-store.dispatch({ type: "filters/statusFilterChanged", payload: "Active" });
-
-store.dispatch({
-  type: "filters/colorFilterChanged",
-  payload: { color: "red", changeType: "added" }
-});
-
-// Stop listening to state updates
-unsubscribe();
-
-// Dispatch one more action to see what happens
-
-store.dispatch({ type: "todos/todoAdded", payload: "Try creating a store" });
+// log: '1'
+// log: '2'
+// log: '3'
 
 ReactDOM.render(
   <React.StrictMode>
