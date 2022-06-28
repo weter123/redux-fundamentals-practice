42a60678c8e73f5ef57848a87d1ede53ef2f47ea
weter123 <dull.x22@gmail.com>
2022-06-28 16:13:21 -0400

    created delayedMessageMiddleware created a middleware that logs action.payload after a delay.

--- src/exampleAddons/middleware.js
+++ src/exampleAddons/middleware.js
@@ -27,3 +27,13 @@ export const alwaysReturnHellowMiddleware = (storeAPI) => (next) => (
   console.log("hello");
   return "hello";
 };
+
+export const delayedMessageMiddleware = (storeAPI) => (next) => (action) => {
+  if (action.type === "todos/todoAdded") {
+    setTimeout(() => {
+      console.log("Added a new todo: ", action.payload);
+    }, 1000);
+  }
+
+  return next(action);
+};
